2b53987d4f7c1ae731d0a6e7c826f2686c022d63
You Wu <rmq350@ku.dk>
2017-10-31 10:52:21 +0100

    Revert "Revert "Added a camera shake script to achieve effect of shaking of all objects.""
    This reverts commit 46e06bea0ba39939f0bea055428ae8aa64492e3e.

---
+++ Assets/Scripts/CameraShake.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+
+    public Transform cameraTransform;
+
+    public float shakeDuration = 0f;
+    public float shakeIntensity = 0.1f;
+
+    Vector3 originalPos;
+    // Use this for initialization
+    void Awake()
+    {
+        if (cameraTransform == null)
+        {
+            cameraTransform = GetComponent<Transform>();
+        }
+		originalPos = cameraTransform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cameraTransform == null)
+        {
+            Debug.LogError("Transform of Camera is NUll!");
+        }
+        else
+        {
+            if (shakeDuration > 0)
+            {
+                cameraTransform.position = originalPos + Random.insideUnitSphere * shakeIntensity;
+                shakeDuration -= Time.deltaTime;
+            }
+            else
+            {
+                shakeDuration = 0f;
+                cameraTransform.position = originalPos;
+            }
+        }
+
+    }
+}
