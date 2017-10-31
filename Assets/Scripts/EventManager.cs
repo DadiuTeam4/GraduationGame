b8469908ea6ab32143d0a418e0da1af00fe45128
IYavin <itai.yavin@gmail.com>
2017-10-31 10:53:17 +0100

    Finished Initial EventManager

--- Assets/Scripts/EventManager.cs
+++ Assets/Scripts/EventManager.cs
@@ -2,21 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum CustomEvent
-{
-	test
-}
-
-public class EventManager : MonoBehaviour 
+public class EventManager : Singleton<EventManager> 
 {
 	private Dictionary<CustomEvent, List<EventDelegate>> listeners = new Dictionary<CustomEvent, List<EventDelegate>>();
 
-	public delegate void EventDelegate();
+	public delegate void EventDelegate(EventArgument argument);
 
 	public void AddListener(CustomEvent eventName, EventDelegate newListener)
 	{
 		List<EventDelegate> eventList;
-		if(listeners.ContainsKey(eventName))
+		if (listeners.ContainsKey(eventName))
 		{
 			listeners.TryGetValue(eventName, out eventList);
 
@@ -25,24 +20,58 @@ public class EventManager : MonoBehaviour
 		else
 		{
 			eventList = new List<EventDelegate>();
+			eventList.Add(newListener);
 			listeners.Add(eventName, eventList);
 		}
 	}
 
-	public bool CallEvent(CustomEvent eventName)
+	public bool RemoveListener(CustomEvent eventName, EventDelegate oldListener)
 	{
-		if(listeners.ContainsKey(eventName))
+		List<EventDelegate> eventList;
+		if (listeners.ContainsKey(eventName))
 		{
-			List<EventDelegate> eventList = new List<EventDelegate>();
+			listeners.TryGetValue(eventName, out eventList);
+
+			foreach (EventDelegate eventDelegate in eventList)
+			{
+				if (eventDelegate == oldListener)
+				{
+					eventList.Remove(eventDelegate);
+					
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
+	public bool CallEvent(CustomEvent eventName, EventArgument argument)
+	{
+		if (listeners.ContainsKey(eventName))
+		{
+			List<EventDelegate> eventList;
+			listeners.TryGetValue(eventName, out eventList);
 			
 			foreach(EventDelegate auto in eventList)
 			{
-				auto();
+				auto(argument);
 			}
 
 			return true;
 		}
 
 		return false;
+	}
+
+	public enum CustomEvent
+	{
+		test
+	}
+
+	public class EventArgument
+	{
+		public string stringComponent = "";
+		public float floatComponent = 0.0f;
 	}
 }
