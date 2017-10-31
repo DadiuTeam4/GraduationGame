9c65815b9cc39972af69bb4c77f935d8aee66773
Unknown <tilemachospantazis8@gmail.com>
2017-10-31 13:24:34 +0100

    Moving fog around PO

--- Assets/StartWind.cs
+++ Assets/StartWind.cs
@@ -6,11 +6,19 @@ public class StartWind : MonoBehaviour {
 
 public GameObject windZoneLeft; //the prefab you want to spawn
 public GameObject windZoneRight; //the prefab you want to spawn
-
+public GameObject middleFog;
 private static float waitTime = 3;
 public float timer = waitTime; 
+private float moveMiddleFogBy = 1;
+private Vector3 originalMiddlePosition;
 
 private GameObject newWind;
+	void Start()
+	{
+		originalMiddlePosition = middleFog.transform.position;		
+	}
+
+
 	void Update ()
 	{
 
@@ -19,6 +27,7 @@ private GameObject newWind;
 		if (Input.GetKeyDown("e"))
 		{
 			Destroy(newWind);
+			middleFog.transform.position = new Vector3(middleFog.transform.position.x - moveMiddleFogBy,middleFog.transform.position.y,middleFog.transform.position.z);
   	       	newWind = Instantiate(windZoneLeft) as GameObject;
 			timer = waitTime;
 		}
@@ -26,6 +35,7 @@ private GameObject newWind;
 		if (Input.GetKeyDown("q"))
 		{
 			Destroy(newWind);
+			middleFog.transform.position = new Vector3(middleFog.transform.position.x + moveMiddleFogBy,middleFog.transform.position.y,middleFog.transform.position.z);			
   	       	newWind = Instantiate(windZoneRight) as GameObject;
 			timer = waitTime;
 		}
@@ -33,6 +43,7 @@ private GameObject newWind;
 		if(timer < 0)
 		{
 			Destroy(newWind);
+			middleFog.transform.position = originalMiddlePosition;
 			timer = waitTime;
 		}
 
