5b17dd76639ced26459e131506c5cf595e35f0b9
Unknown <tilemachospantazis8@gmail.com>
2017-10-31 11:34:23 +0100

    Directional wind

---
+++ Assets/StartWind.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartWind : MonoBehaviour {
+
+public GameObject windZoneLeft; //the prefab you want to spawn
+public GameObject windZoneRight; //the prefab you want to spawn
+
+private static float waitTime = 3;
+public float timer = waitTime; 
+
+private GameObject newWind;
+	void Update ()
+	{
+
+		timer -= Time.deltaTime;
+
+		if (Input.GetKeyDown("e"))
+		{
+			Destroy(newWind);
+  	       	newWind = Instantiate(windZoneLeft) as GameObject;
+			timer = waitTime;
+		}
+
+		if (Input.GetKeyDown("q"))
+		{
+			Destroy(newWind);
+  	       	newWind = Instantiate(windZoneRight) as GameObject;
+			timer = waitTime;
+		}
+
+		if(timer < 0)
+		{
+			Destroy(newWind);
+			timer = waitTime;
+		}
+
+
+    }
+	
+}
