53759cb25b76828b8647a2bd77d1f70e9fdbb732
IYavin <itai.yavin@gmail.com>
2017-11-01 09:07:35 +0100

    WIP sink ground functionality.

--- Assets/Scripts/SinkableGround.cs
+++ Assets/Scripts/SinkableGround.cs
@@ -8,10 +8,8 @@ using UnityEngine;
 [RequireComponent(typeof(MeshCollider))]
 public class SinkableGround : Holdable 
 {
-	[Tooltip("The minimum accepted distance to vertex")]
-	public float accuracy = 1.0f;
 	[Tooltip("The speed of which sinking happens")]
-	[Range(0.01f, 1.0f)]
+	[Range(0.1f, 1.0f)]
 	public float sinkSpeed = 0.1f;
 	[Tooltip("The amount of time before sinking")]
 	public float sinkDelay = 0.0f;
@@ -20,10 +18,17 @@ public class SinkableGround : Holdable
 
 	private MeshCollider meshCollider;
 	private MeshFilter meshFilter;
+	private Mesh mesh;
+
+	private int currentVertexIndex;
 	
 	void Start () {
 		meshCollider = GetComponent<MeshCollider>();
 		meshFilter = GetComponent<MeshFilter>();
+
+		mesh = meshFilter.mesh;
+//		Debug.Log(mesh.vertexCount);
+
 	}
 	
 	void Update () {
@@ -32,30 +37,38 @@ public class SinkableGround : Holdable
 
 	public override void OnTouchBegin(RaycastHit hit) 
 	{
+		Vector3 pointHit = hit.point;
+		pointHit = transform.InverseTransformPoint(pointHit);
 
+		mesh = meshFilter.mesh;
+		Vector3[] vertices = mesh.vertices;
+		Vector3[] normals = mesh.normals;
+
+		Vector3 nearestVertex = vertices[0];
+		float closestDistance = Vector3.Distance(nearestVertex, pointHit);
+
+		float distance;
+		for(int i = 1; i < vertices.Length; i++)
+		{
+			distance = Vector3.Distance(vertices[i], pointHit);
+			if(distance < closestDistance)
+			{
+				nearestVertex = vertices[i];
+				closestDistance = distance;
+				currentVertexIndex = i;
+			}
+		}
 	}
 	
 	public override void OnTouchHold(RaycastHit hit) 
 	{
-		/*if(Time.frameCount % updateRate == 0){
-			Mesh mesh = meshFilter.mesh;
+		if(Time.frameCount % updateRate == 0)
+		{
 			Vector3[] vertices = mesh.vertices;
-			Vector3[] normals = mesh.normals;
-
-			Vector3 nearestVertex = ;
-			float closestDistance = ;
-
-			foreach (Vector3 vertex in vertices)
-			{
-				if(Vector3.Distance(vertex, hit.point) < accuracy)
-				{
-					nearestVertex = vertex;
-
-				}
-			}
+			vertices[currentVertexIndex].y -= sinkSpeed;
 
 			mesh.vertices = vertices;
-		}*/
+		}
 	}
 	
 	public override void OnTouchReleased() 
