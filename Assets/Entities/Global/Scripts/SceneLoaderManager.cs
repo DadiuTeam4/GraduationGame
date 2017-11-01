eb3cb55956f69d930f03d0f0bda16e740a278284
You Wu <rmq350@ku.dk>
2017-11-01 13:44:58 +0100

    added updated SceneLoaderManager to dev branch

--- Assets/Entities/Global/Scripts/SceneLoaderManager.cs
+++ Assets/Entities/Global/Scripts/SceneLoaderManager.cs
@@ -1,23 +1,89 @@
 ﻿//Author: Emil Villumsen
+//Collaborator: Jonathan,
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+using Events;
+
 public class SceneLoaderManager : Singleton<SceneLoaderManager> {
 
-    string[] scenes = { "Crossroads", "IntroLevel", "RitualEvent", "GlobalScene" }; 
+    // Variables to keep track of scenes to load and unload.
+    GameScene previousScene;
+    GameScene currentScene;
+    GameScene nextScene;
+
+    // Clusters of scenes to be loaded at certain points.
+    GameScene[] gameStart = { GameScene.GameOpener, GameScene.GlobalScene, GameScene.IntroLevel, GameScene.CrossRoad1 };
+    GameScene[] gameEnd = { GameScene.EndScene, GameScene.Credits }
 
     void Start()
     {
-        foreach (string sceneName in scenes)
+        SceneClusterLoader(gameStart);
+        EventManager.AddListener(CustomEvent.changeScene, sceneLoader(EventArgument));
+     
+        previousScene = GameScene.GameOpener;
+        currentScene = GameScene.IntroLevel;
+        nextScene = GameScene.CrossRoad1;
+
+
+    }
+
+    // Start the game!
+    private void SceneClusterLoader(GameScene[] cluster)
+    {
+        foreach (GameScene sceneName in cluster)
         {
-            Scene scene = SceneManager.GetSceneByName(sceneName);
+            var sceneToLoad = GameSceneToString(sceneName);
+            Scene scene = SceneManager.GetSceneByName(sceneToLoad);
             if (!scene.isLoaded)
             {
-                SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+                SceneManager.LoadScene(sceneToLoad, LoadSceneMode.Additive);
             }
         }
+    }
+
+    // The main scene changing function. Updates scene trackers and loads and unloads scenes.
+    private void sceneLoader(GameScene upComingScene)
+    {
+        // This system is not correct, we need to find a way to keep track of
+        // upcoming scenes.
+        var sceneToUnload = GameSceneToString(previousScene);
+
+        previousScene = currentScene;
+        currentScene = nextScene;
+        nextScene = upComingScene;
+
+        var sceneToLoad = GameSceneToString(nextScene);
+
+        Scene scene = SceneManager.GetSceneByName(sceneToLoad);
+        if (!scene.isLoaded)
+        {
+            SceneManager.LoadScene(sceneToLoad, LoadSceneMode.Additive);
+        }
+
+        SceneManager.UnloadSceneAsync(sceneToUnload);
+
+    }
+
+    // Takes a GameScene Enum and returns the scene name string.
+    private string GameSceneToString(GameScene sceneName)
+    {
+        return sceneName.ToString("D");
+    }
+
+
+    public enum GameScene
+    {
+        GameOpener,
+        GlobalScene,
+        EndScene,
+        Credits,
+        IntroLevel,
+        CrossRoad1,
+        RitualEvent
+
     }
 
 }
